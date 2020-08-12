9a6107714fc1a6457b5edf37ed3ec3b8348efc43
krzysiek2222 <desmont2222@gmail.com>
2020-08-12 16:27:50 +0200

    Core types (Enum + Tuples)
    x

---
+++ app.js
@@ -0,0 +1,40 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Chris",
+//   age: 25,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: "Chris",
+    age: 25,
+    hobbies: ["Sports", "Cooking"],
+    role: Role.READ_ONLY
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
+}
+else if (person.role === Role.ADMIN) {
+    console.log("is Admin");
+}
+else if (person.role === Role.READ_ONLY) {
+    console.log("is Read onlyr");
+}
+else {
+    console.log("No role found");
+}
